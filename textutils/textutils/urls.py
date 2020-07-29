a6a9617517e224d5e9ad6975b96f94e89f502d60
Yuvi <deshmukhyuvraj@gmail.com>
2020-07-29 17:13:20 +0530

    csrf token added, using post method, code clean

--- textutils/textutils/urls.py
+++ textutils/textutils/urls.py
@@ -1,40 +1,10 @@
-"""textutils URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', views.index, name='index'),
-#     path('about', views.about, name='about'),
-#
-# ]
-
-# text utility
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    # path('remove_punctuation', views.remove_punctuation, name='remove_punctuation'),
-    # path('capitalize_first', views.capitalize_first, name='capitalize_first'),
-    # path('remove_newline', views.remove_newline, name='remove_newline'),
-    # path('remove_space', views.remove_space, name='remove_space'),
-    # path('char_count', views.char_count, name='char_count'),
-    path('analyze/', views.analyze, name='analyze'),
-    path('e1/', views.e1, name='e1'),
-
+    path('analyze', views.analyze, name='analyze'),
 ]
--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -5,90 +5,64 @@ from django.shortcuts import render
 
 
 def index(request):
-    # return HttpResponse("Home")
-    #  templates uses
     return render(request, 'index.html')
 
 
 def analyze(request):
-    keys_to_get = ['remove_punctuation', 'uppercase', 'remove_newline', 'remove_extra_space', 'char_count']
-    remove_punctuation, uppercase, remove_newline, remove_extra_space, char_count = [request.GET.get(key) for key
-                                                                                     in keys_to_get]
-    djtext = request.GET.get('text', 'default')
+    keys_to_post = ['remove_punctuation', 'uppercase', 'remove_newline', 'remove_extra_space', 'char_count']
+    remove_punctuation, uppercase, remove_newline, remove_extra_space, char_count = [request.POST.get(key) for key
+                                                                                     in keys_to_post]
+    inputText_original = request.POST.get('text', 'default')
+    inputText = inputText_original
     result_text = ""
-    # remove_punctuation = request.GET.get('remove_punctuation', 'default')
-    # uppercase = request.GET.get('uppercase', 'default')
-    # remove_newline = request.GET.get('remove_newline', 'default')
-    # remove_extra_space = request.GET.get('remove_extra_space', 'default')
-    # char_count = request.GET.get('char_count', 'default')
-    # djlist = request.GET.getlist('remove_punctuation', 'uppercase', 'remove_newline')
-    # print(djlist)
-    # print(remove_punctuation)
 
     if remove_punctuation == 'on':
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
 
-        for char in djtext:
+        for char in inputText:
             if char not in punctuations:
                 analyzed = analyzed + char
 
-        djtext = analyzed
+        inputText = analyzed
         result_text = 'Punctuations removed\n'
         params = {'purpose': result_text, 'analyzed_text': analyzed}
 
-        # return render(request, 'analyze.html', params)
-
     if uppercase == 'on':
-        analyzed = djtext.upper()
-        djtext = analyzed
-        result_text = result_text + 'text converted to Uppercase\n'
+        analyzed = inputText.upper()
+        inputText = analyzed
+        result_text = result_text + 'Converted to uppercase\n'
         params = {'purpose': result_text, 'analyzed_text': analyzed}
         # return render(request, 'analyze.html', params)
 
     if remove_newline == 'on':
         analyzed = ""
-        for char in djtext:
-            if char != "\n":
+        for char in inputText:
+            if char != "\n" and char != "\r":
                 analyzed = analyzed + char
-        djtext = analyzed
+        inputText = analyzed
         result_text = result_text + 'Newline removed\n'
         params = {'purpose': result_text, 'analyzed_text': analyzed}
-        # return render(request, 'analyze.html', params)
 
     if remove_extra_space == 'on':
         analyzed = ""
-        for index, char in enumerate(djtext):
-            if not (djtext[index] == " " and djtext[index + 1] == " "):
+        for index, char in enumerate(inputText):
+            if not (inputText[index] == " " and inputText[index + 1] == " "):
                 analyzed = analyzed + char
-        djtext = analyzed
-        result_text = result_text + 'Extra Space Removed\n'
+        inputText = analyzed
+        result_text = result_text + 'Extra space removed\n'
         params = {'purpose': result_text, 'analyzed_text': analyzed}
-        # return render(request, 'analyze.html', params)
 
     if char_count == 'on':
-        analyzed = analyzed + '\nCharacter count = ' + str(len(djtext))
-        # for index, char in enumerate(djtext):
-        #     if not (djtext[index] == " " and djtext[index+1] == " "):
-        #         analyzed = analyzed + char
-        # analyzed = analyzed
-        result_text = result_text + 'Character Count\n'
+        analyzed = analyzed + '\nCharacter count = ' + str(len(inputText_original))
+        result_text = result_text + 'Character counted\n'
         params = {'purpose': result_text, 'analyzed_text': analyzed}
-    #     return render(request, 'analyze.html', params)
 
-    if remove_punctuation or uppercase or remove_newline or remove_extra_space:
-        return render(request, 'analyze.html', params)
+    if inputText == "":
+        return HttpResponse('Please enter the text and try again.')
 
     else:
-        return HttpResponse('Error')
-
-    # analyze text
-    # return HttpResponse("remove_punctuation")
-
-
-# Example 1
-
-def e1(request):
-    returnString = '''<h1>Entertainment : </h1><a href = https://www.google.com>Sab Milega</a>
-    <h1>Social Media : </h1><a href = https://www.facebook.com>Facebook</a>'''
-    return HttpResponse(returnString)
+        if remove_punctuation or uppercase or remove_newline or remove_extra_space:
+            return render(request, 'analyze.html', params)
+        else:
+            return HttpResponse('Please select any one operation and try again.....')
